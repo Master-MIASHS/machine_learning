ad5a566c0b7c8ae542fb4850353bad8c8097128e
maximiliense <maximiliense@users.noreply.github.com>
2026-08-14 15:00:15 +0000

    deploy: 3bacfd0f2b59b62bea274343b1a93838564246d7

---
+++ _app/immutable/entry/start.Cveeerlf.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/B6-fXcV3.js";import{o as t}from"../chunks/DchTGYcM.js";export{t as load_css,r as start};
--- _app/immutable/entry/start.D2KGYUZZ.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/j0sZezt9.js";import{o as t}from"../chunks/I8hCUjra.js";export{t as load_css,r as start};
